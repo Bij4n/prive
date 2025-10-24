204fb05437e56c0e86128ff31ea6abcb246b1812
Bij4n <b@mawji.vc>
2025-10-24 19:54:39 -0400

    implement shell completions: bash, zsh, fish, powershell

---
+++ src/commands/completions.rs
@@ -0,0 +1,22 @@
+use std::io;
+
+use anyhow::Result;
+use clap::CommandFactory;
+use clap_complete::{generate, Shell};
+
+use crate::cli::{Cli, CompletionsArgs, ShellType};
+
+pub fn handle_completions(args: &CompletionsArgs) -> Result<()> {
+    let shell = match args.shell {
+        ShellType::Bash => Shell::Bash,
+        ShellType::Zsh => Shell::Zsh,
+        ShellType::Fish => Shell::Fish,
+        ShellType::Powershell => Shell::PowerShell,
+    };
+
+    let mut cmd = Cli::command();
+    let name = cmd.get_name().to_string();
+    generate(shell, &mut cmd, name, &mut io::stdout());
+
+    Ok(())
+}
