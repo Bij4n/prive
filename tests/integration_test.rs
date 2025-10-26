0d31a14fcfad84238e3017a4a83c73f6c5bb3fe6
Bij4n <b@mawji.vc>
2025-10-26 17:21:59 -0400

    add integration test: generate default password

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -16,3 +16,22 @@ fn test_cli_version() {
     Command::cargo_bin("prive")
         .unwrap()
         .arg("--version")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("prive"));
+}
+
+#[test]
+fn test_generate_default() {
+    Command::cargo_bin("prive")
+        .unwrap()
+        .arg("generate")
+        .assert()
+        .success()
+        .stdout(predicate::str::is_match("[a-zA-Z0-9!@#$%^&*()\\-_=+\\[\\]{}|;:,.<>?]{20}").unwrap());
+}
+
+#[test]
+fn test_generate_custom_length() {
+    Command::cargo_bin("prive")
+        .unwrap()
